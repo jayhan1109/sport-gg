845810030d6752f08301e79fa3213109c2f5839c
Jay Han <jungho1109@gmail.com>
2020-07-17 09:53:43 +0900

    fix error in server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.get("/", (res, res) => {
-  res.setEncoding("Root");
+  res.send("Root");
 });
 
 // Serve static assets in production

cfb5acca3065b04a2d259750282998519b1fc36f
Jay Han <jungho1109@gmail.com>
2020-07-17 09:45:58 +0900

    heroku setup

--- server.js
+++ server.js
@@ -7,6 +7,16 @@ app.get("/", (res, res) => {
   res.setEncoding("Root");
 });
 
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(port, (req, res) => {
   console.log(`Server is ON ${port}`);
 });

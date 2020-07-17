0e2a05d746e3f5fca6f96f9da9e4244e78dd1f5e
Jay Han <jungho1109@gmail.com>
2020-07-17 17:32:52 +0900

    Standing Route

--- routes/api/standing.js
+++ routes/api/standing.js
@@ -1,8 +1,33 @@
 const express = require("express");
 const router = express.Router();
+const fs = require("fs");
+const path = require("path");
 
-router.get("/", (req, res) => {
-  res.send("standing");
+// @route   put     /api/standing/:league/:season
+// @desc    Get league standing of season
+// @access  Public
+router.get("/:league/:season", (req, res) => {
+  const data = {};
+  fs.readFile(
+    path.join(
+      __dirname,
+      `../../api/standing/${req.params.league}/${req.params.season}_standing.json`
+    ),
+    "utf8",
+    (err, jsonFile) => {
+      if (err) throw new Error(err);
+      const jsonData = JSON.parse(jsonFile);
+      const result = jsonData.response[0];
+
+      data.leagueName = result.league.name;
+      data.leagueLogo = result.league.logo;
+      data.standings = result.league.standings[0];
+
+      console.log(data);
+
+      res.json(data);
+    }
+  );
 });
 
 module.exports = router;

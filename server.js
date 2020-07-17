342e5e55ef742d6c4c7db4af04f3c38f0cbadf87
Jay Han <jungho1109@gmail.com>
2020-07-17 14:11:49 +0900

    api & router initial setup

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require("express");
+const userRouter = require("./routes/auth/user");
+const standingRouter = require("./routes/api/standing");
+const teamsRouter = require("./routes/api/teams");
+const topscoreRouter = require("./routes/api/topscore");
+const connectDB = require("./config/db");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Root");
-});
+// Connect Database
+connectDB();
+
+app.use(express.json({ extended: false }));
+
+app.use("/auth", userRouter);
+app.use("/api/standing", standingRouter);
+app.use("/api/teams", teamsRouter);
+app.use("/api/topscore", topscoreRouter);
 
 // Serve static assets in production
 if (process.env.NODE_ENV === "production") {

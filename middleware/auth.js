e0fff784e4c56b3c33cbae380c5675f3366f6a2f
Jay Han <jungho1109@gmail.com>
2020-07-17 16:53:22 +0900

    Register, Login, Load user

---
+++ middleware/auth.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+const auth = (req, res, next) => {
+  // Get token from header
+  const token = req.header("x-auth-token");
+
+  // Check if token exists
+  if (!token) {
+    return res.status(401).json({ msg: "No token, authorization denied" });
+  }
+
+  // Verify token
+  try {
+    const decodedToken = jwt.verify(token, config.get("jwtToken"));
+    req.user = decodedToken;
+    console.log(req.user);
+    next();
+  } catch (e) {
+    res.status(401).json({ msg: "Token is not valid" });
+  }
+};
+
+module.exports = auth;
--- routes/auth/user.js
+++ routes/auth/user.js
@@ -1,8 +1,150 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const auth = require("../../middleware/auth");
 
-router.get("/", (req, res) => {
-  res.send("user");
+const User = require("../../models/User");
+const { check, validationResult } = require("express-validator");
+
+// @route   POST     /auth/login
+// @desc    Register User
+// @access  Public
+router.post(
+  "/login",
+  [
+    check("email", "Please enter a valid email").isEmail(),
+    check("password", "Password is required").exists(),
+  ],
+  async (req, res) => {
+    // Check express-validation
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      // See if user exists
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
+      }
+
+      // Check if password valid
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
+      }
+
+      // Create token
+      jwt.sign(
+        { id: user.id },
+        config.get("jwtToken"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (e) {
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+// @route   POST     /auth/register
+// @desc    Register User
+// @access  Public
+router.post(
+  "/register",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please enter a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    // Check express-validation
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // Destructing
+    const { name, email, password } = req.body;
+
+    try {
+      // Check if the email has already been used
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
+      }
+
+      // Create new user
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      // Encrypt password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      // Save user
+      await user.save();
+
+      // Create token
+      jwt.sign(
+        { id: user.id },
+        config.get("jwtToken"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (e) {
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+// @route   POST     /auth
+// @desc    Load User
+// @access  Private
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (e) {
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route   put     /auth
+// @desc    Edit username
+// @access  Private
+router.put("/", async (req, res) => {
+  res.send("Edit Username");
+});
+
+// @route   put     /auth/bookmark
+// @desc    Update Bookmark
+// @access  Private
+router.put("/bookmark", async (req, res) => {
+  res.send("Update bookmark");
 });
 
 module.exports = router;

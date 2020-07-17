7fbb5bf70c2f321656071601d94d74f5a06d84e7
Jay Han <jungho1109@gmail.com>
2020-07-17 09:57:18 +0900

    fix Error in Server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get("/", (res, res) => {
+app.get("/", (req, res) => {
   res.send("Root");
 });
 

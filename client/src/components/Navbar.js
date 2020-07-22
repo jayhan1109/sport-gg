35900a62bfa58fa7253dfbd3be869ef6d953f118
Jay Han <jungho1109@gmail.com>
2020-07-22 15:45:55 +0900

    Logout

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,43 @@
 import React, { Fragment } from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import "../styles/Navbar.scss";
+import { useRecoilValue, useRecoilState } from "recoil";
+import { authState, userState } from "../recoil/auth";
+import { alertState } from "../recoil/alert";
 
 const Navbar = () => {
-  console.log("y");
+  const [auth, setAuth] = useRecoilState(authState);
+  const [user, setUser] = useRecoilState(userState);
+  const [alert, setAlert] = useRecoilState(alertState);
+
+  const logout = (e) => {
+    e.preventDefault();
+    setAuth(false);
+    setUser("");
+    localStorage.removeItem("token");
+    setAlert({ msg: "Logout Success", type: "success" });
+    return <Redirect to="/" />;
+  };
+
+  if (auth) {
+    return (
+      <Fragment>
+        <div className="navbar">
+          <div className="navbar-title">
+            <Link className="navbar-title-nav" to="/">
+              SportGG
+            </Link>
+          </div>
+          <div className="navbar-navs">
+            <Link className="navbar-navs-nav" onClick={logout}>
+              Logout
+            </Link>
+          </div>
+        </div>
+      </Fragment>
+    );
+  }
+
   return (
     <Fragment>
       <div className="navbar">
